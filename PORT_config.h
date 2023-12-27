b62ef8f4a700ce3a175d348ee0e6bc708443cd55
taher mohamed <107802873+taher77777@users.noreply.github.com>
2023-12-27 20:44:10 +0200

    Add files via upload

---
+++ PORT_config.h
@@ -0,0 +1,96 @@
+/***********************************************************************/
+/***********************************************************************/
+/*****************    AUTHOR: TAHER MOHAMED   **************************/
+/*****************    LAYER:  MCAL            **************************/
+/*****************    SWC:    DIO             **************************/
+/*****************    VERSION:1.00            **************************/
+/***********************************************************************/
+/***********************************************************************/
+#ifndef PORT_CONFIG_H_
+#define PORT_CONFIG_H_
+
+/*choose :  
+		  1 for OUTPUT
+		  0 for INPUT           */
+#define PORTA_PIN0_DIR               0
+#define PORTA_PIN1_DIR               0
+#define PORTA_PIN2_DIR               0
+#define PORTA_PIN3_DIR               0
+#define PORTA_PIN4_DIR               0
+#define PORTA_PIN5_DIR               0
+#define PORTA_PIN6_DIR               0
+#define PORTA_PIN7_DIR               0
+                                    
+#define PORTB_PIN0_DIR               0
+#define PORTB_PIN1_DIR               0
+#define PORTB_PIN2_DIR               0
+#define PORTB_PIN3_DIR               0
+#define PORTB_PIN4_DIR               0
+#define PORTB_PIN5_DIR               0
+#define PORTB_PIN6_DIR               0
+#define PORTB_PIN7_DIR               0
+                                    
+                                    
+#define PORTC_PIN0_DIR               0
+#define PORTC_PIN1_DIR               0
+#define PORTC_PIN2_DIR               0
+#define PORTC_PIN3_DIR               0
+#define PORTC_PIN4_DIR               0
+#define PORTC_PIN5_DIR               0
+#define PORTC_PIN6_DIR               0
+#define PORTC_PIN7_DIR               0
+                                    
+#define PORTD_PIN0_DIR               0
+#define PORTD_PIN1_DIR               0
+#define PORTD_PIN2_DIR               0
+#define PORTD_PIN3_DIR               0
+#define PORTD_PIN4_DIR               0
+#define PORTD_PIN5_DIR               0
+#define PORTD_PIN6_DIR               0
+#define PORTD_PIN7_DIR               0
+
+
+
+/*choose: 0 for floating if input,low if output   
+          1 for pullup   if input,high if output
+*/
+
+#define PORTA_PIN0_INITIAL_VALUE     0
+#define PORTA_PIN1_INITIAL_VALUE     0
+#define PORTA_PIN2_INITIAL_VALUE     0
+#define PORTA_PIN3_INITIAL_VALUE     0
+#define PORTA_PIN4_INITIAL_VALUE     0
+#define PORTA_PIN5_INITIAL_VALUE     0
+#define PORTA_PIN6_INITIAL_VALUE     0
+#define PORTA_PIN7_INITIAL_VALUE     0
+                               
+#define PORTB_PIN0_INITIAL_VALUE     0
+#define PORTB_PIN1_INITIAL_VALUE     0
+#define PORTB_PIN2_INITIAL_VALUE     0
+#define PORTB_PIN3_INITIAL_VALUE     0
+#define PORTB_PIN4_INITIAL_VALUE     0
+#define PORTB_PIN5_INITIAL_VALUE     0
+#define PORTB_PIN6_INITIAL_VALUE     0
+#define PORTB_PIN7_INITIAL_VALUE     0
+                           
+#define PORTC_PIN0_INITIAL_VALUE     0
+#define PORTC_PIN1_INITIAL_VALUE     0
+#define PORTC_PIN2_INITIAL_VALUE     0
+#define PORTC_PIN3_INITIAL_VALUE     0
+#define PORTC_PIN4_INITIAL_VALUE     0
+#define PORTC_PIN5_INITIAL_VALUE     0
+#define PORTC_PIN6_INITIAL_VALUE     0
+#define PORTC_PIN7_INITIAL_VALUE     0
+                            
+#define PORTD_PIN0_INITIAL_VALUE     0
+#define PORTD_PIN1_INITIAL_VALUE     0
+#define PORTD_PIN2_INITIAL_VALUE     0
+#define PORTD_PIN3_INITIAL_VALUE     0
+#define PORTD_PIN4_INITIAL_VALUE     0
+#define PORTD_PIN5_INITIAL_VALUE     0
+#define PORTD_PIN6_INITIAL_VALUE     0
+#define PORTD_PIN7_INITIAL_VALUE     0
+
+
+
+#endif
